27435d4b3a75f526ac626b58156d230eae6010cb
Todor Chenkov <joto_to@mail.bg>
2017-11-07 22:18:41 +0200

    -add P01_Vehicles

--- src/L08_Polymorphism_Exercises/P01_Vehicles/Main.java
+++ src/L08_Polymorphism_Exercises/P01_Vehicles/Main.java
@@ -12,16 +12,30 @@ public class Main {
         Vehicle truck = getVehicle(br);
         
         travel(car, truck, br);
+    
+        System.out.println(car);
+        System.out.println(truck);
     }
     
     private static void travel(Vehicle car, Vehicle truck, BufferedReader br) throws IOException {
         int commandsCount = Integer.parseInt(br.readLine());
-    
+        
         for (int i = 0; i < commandsCount; i++) {
             String[] command = br.readLine().split(" ");
+            Vehicle currentVehicle = command[1].equals("Car") ? car : truck;
+            double distance = Double.parseDouble(command[2]);
             
-            if (command[0].equals("Drive")){
-            
+            switch (command[0]){
+                case "Drive":
+                    try {
+                        currentVehicle.drive(distance);
+                    } catch (IllegalArgumentException iae) {
+                        System.out.println(iae.getMessage());
+                    }
+                    break;
+                case "Refuel":
+                    double fuel = Double.parseDouble(command[2]);
+                    currentVehicle.refuel(fuel);
             }
         }
     }
--- src/L08_Polymorphism_Exercises/P01_Vehicles/Vehicle.java
+++ src/L08_Polymorphism_Exercises/P01_Vehicles/Vehicle.java
@@ -1,5 +1,7 @@
 package L08_Polymorphism_Exercises.P01_Vehicles;
 
+import java.text.DecimalFormat;
+
 public class Vehicle {
     private static final double INITIAL_DISTANCE_TRAVELED = 0;
     
@@ -25,17 +27,32 @@ public class Vehicle {
         this.distanceTraveled = distanceTraveled;
     }
     
-    void drive(double distance){
+    void drive(double distance) {
         double fuelNeeded = distance * this.fuelConsumptionInLitersPerKm;
-        if (this.fuelQuantity < fuelNeeded){
+        if (this.fuelQuantity < fuelNeeded) {
             throw new IllegalArgumentException(this.getClass().getSimpleName() + " needs refueling");
         }
         setDistanceTraveled(this.distanceTraveled + distance);
         setFuelQuantity(this.fuelQuantity - fuelNeeded);
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println(
+                String.format("%s travelled %s km",
+                        this.getClass().getSimpleName(),
+                        df.format(distance)
+                )
+        );
     }
     
-    void refuel(double refuelQuantity){
+    void refuel(double refuelQuantity) {
         setFuelQuantity(this.fuelQuantity + refuelQuantity);
+        
     }
     
+    
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f",
+                this.getClass().getSimpleName(),
+                this.fuelQuantity);
+    }
 }
